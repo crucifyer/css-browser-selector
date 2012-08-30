51701f6c124dc5eb6396bc4e476a9bb22ecdcbc6
crucify <shj@xenosi.de>
2012-08-30 17:20:32 +0900

    ie9 and more

--- css_browser_selector.js
+++ css_browser_selector.js
@@ -24,9 +24,10 @@ function css_browser_selector(u) {
 		m = 'mobile';
 	return [
 /* IE */
-		(!(/opera|webtv/i.test(ua)) && /msie\s(\d)/.test(ua) && (v = RegExp.$1)) ?
-			('ie ie' + v + ((v == '6' || v == '7') ?
-				' ie67 ie678' : (v == '8') ? ' ie678' : '')) :
+		(!(/opera|webtv/i.test(ua)) && /msie\s(\d)/.test(ua) && (v = RegExp.$1 * 1)) ?
+			('ie ie' + v + ((v == 6 || v == 7) ?
+				' ie67 ie678' : (v == 8) ?
+					' ie678' : (v > 8) ? ' ie9m' : '')) :
 /* FF */
 		(/firefox\/(\d+)\.(\d+)/.test(ua) && (re = RegExp)) ? g + ' ff' + re.$1 + ' ff' + re.$1 + '_' + re.$2 :
 			is('gecko/') ? g :
