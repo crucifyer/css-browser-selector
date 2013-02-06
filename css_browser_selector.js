042101f798bfbcd2bf8e0a0f4cb9f49133fca297
crucify <shj@xenosi.de>
2013-02-06 12:28:35 +0900

    ie6789

--- css_browser_selector.js
+++ css_browser_selector.js
@@ -27,8 +27,8 @@ function css_browser_selector(u) {
 /* IE */
 		(!(/opera|webtv/i.test(ua)) && /msie\s(\d)/.test(ua) && (v = RegExp.$1 * 1)) ?
 			('ie ie' + v + ((v == 6 || v == 7) ?
-				' ie67 ie678' : (v == 8) ?
-					' ie678' : (v > 8) ? ' ie6789 ie9m' : '')) :
+				' ie67 ie678 ie6789' : (v == 8) ?
+					' ie678 ie6789' : (v > 8) ? ' ie6789 ie9m' : '')) :
 /* FF */
 		(/firefox\/(\d+)\.(\d+)/.test(ua) && (re = RegExp)) ? g + ' ff ff' + re.$1 + ' ff' + re.$1 + '_' + re.$2 :
 			is('gecko/') ? g :
