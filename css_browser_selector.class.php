a825f0d73955c7f40bd9c45fee4e07494911b97b
crucify <shj@xenosi.de>
2013-12-02 15:20:27 +0900

    go

--- css_browser_selector.class.php
+++ css_browser_selector.class.php
@@ -10,7 +10,8 @@ $className = css_browser_selector::getClassName($_SERVER['HTTP_USER_AGENT']);
 
 */
 
-class css_browser_selector {
+class css_browser_selector
+{
 	private static $ua = null, $re = null;
 	const g = 'gecko',
 		w = 'webkit',
@@ -19,7 +20,8 @@ class css_browser_selector {
 		o = 'opera',
 		m = 'mobile';
 
-	private function __construct() {}
+	private function __construct() {
+	}
 
 	private static function is($type) {
 		return strpos(self::$ua, $type) !== false ? true : false;
@@ -33,48 +35,51 @@ class css_browser_selector {
 		self::$ua = strtolower($userAgent);
 
 		return implode(' ', array(
-/* IE */
-			(!self::test('~opera|webtv~') && self::test('~msie\s(\d+)~')) ?
-				'ie ie'.self::$re[1].((self::$re[1] == 6 || self::$re[1] == 7) ?
+			/* IE */
+			(! self::test('~opera|webtv~') && self::test('~msie\s(\d+)~')) ?
+				'ie ie' . self::$re[1] . ((self::$re[1] == 6 || self::$re[1] == 7) ?
 					' ie67 ie678 ie6789' : ((self::$re[1] == 8) ?
 						' ie678 ie6789' : ((self::$re[1] == 9) ?
 							' ie6789 ie9m' : ((self::$re[1] > 9) ?
 								' ie9m' : '')))) :
-/* FF */
-			(self::test('~firefox/(\d+)\.(\d+)~') ? self::g.' ff ff'.self::$re[1].' ff'.self::$re[1].'_'.self::$re[2] :
-				(self::is('gecko/') ? self::g :
-/* Opera */
-			(self::is(self::o) ? self::o.(self::test('~version/(\d+)~') ? ' '.self::o.self::$re[1] :
-				(self::test('~opera(\s|/)(\d+)~') ? ' '.self::o.self::$re[2] : '')) :
-/* K */
-			(self::is('konqueror') ? 'konqueror' :
-/* Black Berry */
-			(self::is('blackberry') ? self::m.' blackberry' :
-/* Chrome */
-			((self::is(self::c) || self::is('crios')) ? self::w.' '.self::c :
-/* Iron */
-			(self::is('iron') ? self::w.' iron' :
-/* Safari */
-			(!self::is('cpu os') && self::is('applewebkit/') ? self::w.' '.self::s :
-/* Mozilla */
-			(self::is('mozilla') ? self::g : ''))))))))),
-/* Android */
-			(self::is('android') ? self::m.' android' : ''),
-/* Tablet */
+				/* IE 11 */
+				(self::test('~trident\/\d+.*?;\s*rv:(\d+)\.(\d+)\)~') ?
+					'ie ie' . self::$re[1] . ' ie' . self::$re[1] . '_' . self::$re[2] . ' ie9m' :
+					/* FF */
+					(self::test('~firefox/(\d+)\.(\d+)~') ? self::g . ' ff ff' . self::$re[1] . ' ff' . self::$re[1] . '_' . self::$re[2] :
+						(self::is('gecko/') ? self::g :
+							/* Opera */
+							(self::is(self::o) ? self::o . (self::test('~version/(\d+)~') ? ' ' . self::o . self::$re[1] :
+									(self::test('~opera(\s|/)(\d+)~') ? ' ' . self::o . self::$re[2] : '')) :
+								/* K */
+								(self::is('konqueror') ? 'konqueror' :
+									/* Black Berry */
+									(self::is('blackberry') ? self::m . ' blackberry' :
+										/* Chrome */
+										((self::is(self::c) || self::is('crios')) ? self::w . ' ' . self::c :
+											/* Iron */
+											(self::is('iron') ? self::w . ' iron' :
+												/* Safari */
+												(! self::is('cpu os') && self::is('applewebkit/') ? self::w . ' ' . self::s :
+													/* Mozilla */
+													(self::is('mozilla') ? self::g : '')))))))))),
+			/* Android */
+			(self::is('android') ? self::m . ' android' : ''),
+			/* Tablet */
 			(self::is('tablet') ? 'tablet' : ''),
-/* Machine */
-			(self::is('j2me') ? self::m.' j2me' :
-			(self::is('ipad; u; cpu os') ? self::m.' chrome android tablet' :
-			(self::is('ipad;u;cpu os') ? self::m + ' chromedef android tablet' :
-			(self::is('iphone') ? self::m.' ios iphone' :
-			(self::is('ipod') ? self::m.' ios ipod' :
-			(self::is('ipad') ? self::m.' ios ipad' :
-			(self::is('mac') ? 'mac' :
-			(self::is('darwin') ? 'mac' :
-			(self::is('webtv') ? 'webtv' :
-			(self::is('win') ? 'win'.(self::is('windows nt 6.0') ? ' vista' : '') :
-			(self::is('freebsd') ? 'freebsd' :
-			((self::is('x11') || self::is('linux')) ? 'linux' : ''))))))))))))
+			/* Machine */
+			(self::is('j2me') ? self::m . ' j2me' :
+				(self::is('ipad; u; cpu os') ? self::m . ' chrome android tablet' :
+					(self::is('ipad;u;cpu os') ? self::m . ' chromedef android tablet' :
+						(self::is('iphone') ? self::m . ' ios iphone' :
+							(self::is('ipod') ? self::m . ' ios ipod' :
+								(self::is('ipad') ? self::m . ' ios ipad' :
+									(self::is('mac') ? 'mac' :
+										(self::is('darwin') ? 'mac' :
+											(self::is('webtv') ? 'webtv' :
+												(self::is('win') ? 'win' . (self::is('windows nt 6.0') ? ' vista' : '') :
+													(self::is('freebsd') ? 'freebsd' :
+														((self::is('x11') || self::is('linux')) ? 'linux' : ''))))))))))))
 		));
 	}
 }
---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once 'css_browser_selector.class.php';
+
+echo css_browser_selector::getClassName('Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E; rv:11.0) like Gecko');
+
+?>
