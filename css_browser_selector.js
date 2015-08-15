a39ac5d89c807a7ca3b6e0e97b5796116f403ae0
crucify <shj@xenosi.de>
2015-08-15 21:10:52 +0900

    EDGE

--- css_browser_selector.js
+++ css_browser_selector.js
@@ -32,28 +32,31 @@ function css_browser_selector(u) {
 				' ie678 ie6789' : (v == 9) ?
 				' ie6789 ie9m' : (v > 9 ) ?
 				' ie9m' : '')) :
-			/* IE 11 */
-			(/trident\/\d+.*?;\s*rv:(\d+)\.(\d+)\)/.test(ua) && (v = [RegExp.$1, RegExp.$2])) ?
-				'ie ie' + v[0] + ' ie' + v[0] + '_' + v[1] + ' ie9m' :
-				/* FF */
-				(/firefox\/(\d+)\.(\d+)/.test(ua) && (re = RegExp)) ?
-					g + ' ff ff' + re.$1 + ' ff' + re.$1 + '_' + re.$2 :
-					is('gecko/') ? g :
-						/* Opera */
-						is(o) ? o + (/version\/(\d+)/.test(ua) ? ' ' + o + RegExp.$1 :
-							(/opera(\s|\/)(\d+)/.test(ua) ? ' ' + o + RegExp.$2 : '')) :
-							/* K */
-							is('konqueror') ? 'konqueror' :
-								/* Black Berry */
-								is('blackberry') ? m + ' blackberry' :
-									/* Chrome */
-									(is(c) || is('crios')) ? w + ' ' + c :
-										/* Iron */
-										is('iron') ? w + ' iron' :
-											/* Safari */
-											!is('cpu os') && is('applewebkit/') ? w + ' ' + s :
-												/* Mozilla */
-												is('mozilla/') ? g : '',
+			/* EDGE */
+			(/Edge\/(\d+)\.(\d+)/.test(ua) && (v = [RegExp.$1, RegExp.$2])) ?
+			'ie ie' + v[0] + ' ie' + v[0] + '_' + v[1] + ' ie9m edge' :
+				/* IE 11 */
+				(/trident\/\d+.*?;\s*rv:(\d+)\.(\d+)\)/.test(ua) && (v = [RegExp.$1, RegExp.$2])) ?
+					'ie ie' + v[0] + ' ie' + v[0] + '_' + v[1] + ' ie9m' :
+					/* FF */
+					(/firefox\/(\d+)\.(\d+)/.test(ua) && (re = RegExp)) ?
+						g + ' ff ff' + re.$1 + ' ff' + re.$1 + '_' + re.$2 :
+						is('gecko/') ? g :
+							/* Opera */
+							is(o) ? o + (/version\/(\d+)/.test(ua) ? ' ' + o + RegExp.$1 :
+								(/opera(\s|\/)(\d+)/.test(ua) ? ' ' + o + RegExp.$2 : '')) :
+								/* K */
+								is('konqueror') ? 'konqueror' :
+									/* Black Berry */
+									is('blackberry') ? m + ' blackberry' :
+										/* Chrome */
+										(is(c) || is('crios')) ? w + ' ' + c :
+											/* Iron */
+											is('iron') ? w + ' iron' :
+												/* Safari */
+												!is('cpu os') && is('applewebkit/') ? w + ' ' + s :
+													/* Mozilla */
+													is('mozilla/') ? g : '',
 		/* Android */
 		is('android') ? m + ' android' : '',
 		/* Tablet */
