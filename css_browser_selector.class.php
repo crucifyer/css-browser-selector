c93e785e37bc366eeea31a5250fee3e3107d00a5
crucify <shj@xenosi.de>
2015-08-17 15:32:02 +0900

    php EDGE

--- css_browser_selector.class.php
+++ css_browser_selector.class.php
@@ -42,27 +42,30 @@ class css_browser_selector
 						' ie678 ie6789' : ((self::$re[1] == 9) ?
 							' ie6789 ie9m' : ((self::$re[1] > 9) ?
 								' ie9m' : '')))) :
-				/* IE 11 */
-				(self::test('~trident\/\d+.*?;\s*rv:(\d+)\.(\d+)\)~') ?
-					'ie ie' . self::$re[1] . ' ie' . self::$re[1] . '_' . self::$re[2] . ' ie9m' :
-					/* FF */
-					(self::test('~firefox/(\d+)\.(\d+)~') ? self::g . ' ff ff' . self::$re[1] . ' ff' . self::$re[1] . '_' . self::$re[2] :
-						(self::is('gecko/') ? self::g :
-							/* Opera */
-							(self::is(self::o) ? self::o . (self::test('~version/(\d+)~') ? ' ' . self::o . self::$re[1] :
-									(self::test('~opera(\s|/)(\d+)~') ? ' ' . self::o . self::$re[2] : '')) :
-								/* K */
-								(self::is('konqueror') ? 'konqueror' :
-									/* Black Berry */
-									(self::is('blackberry') ? self::m . ' blackberry' :
-										/* Chrome */
-										((self::is(self::c) || self::is('crios')) ? self::w . ' ' . self::c :
-											/* Iron */
-											(self::is('iron') ? self::w . ' iron' :
-												/* Safari */
-												(! self::is('cpu os') && self::is('applewebkit/') ? self::w . ' ' . self::s :
-													/* Mozilla */
-													(self::is('mozilla') ? self::g : '')))))))))),
+				/* EDGE */
+				(self::test('~edge/(\d+)\.(\d+)~') ?
+					'ie ie' . self::$re[1] . ' ie' . self::$re[1] . '_' . self::$re[2] . ' ie9m edge' :
+					/* IE 11 */
+					(self::test('~trident/\d+.*?;\s*rv:(\d+)\.(\d+)\)~') ?
+						'ie ie' . self::$re[1] . ' ie' . self::$re[1] . '_' . self::$re[2] . ' ie9m' :
+						/* FF */
+						(self::test('~firefox/(\d+)\.(\d+)~') ? self::g . ' ff ff' . self::$re[1] . ' ff' . self::$re[1] . '_' . self::$re[2] :
+							(self::is('gecko/') ? self::g :
+								/* Opera */
+								(self::is(self::o) ? self::o . (self::test('~version/(\d+)~') ? ' ' . self::o . self::$re[1] :
+										(self::test('~opera(\s|/)(\d+)~') ? ' ' . self::o . self::$re[2] : '')) :
+									/* K */
+									(self::is('konqueror') ? 'konqueror' :
+										/* Black Berry */
+										(self::is('blackberry') ? self::m . ' blackberry' :
+											/* Chrome */
+											((self::is(self::c) || self::is('crios')) ? self::w . ' ' . self::c :
+												/* Iron */
+												(self::is('iron') ? self::w . ' iron' :
+													/* Safari */
+													(! self::is('cpu os') && self::is('applewebkit/') ? self::w . ' ' . self::s :
+														/* Mozilla */
+														(self::is('mozilla') ? self::g : ''))))))))))),
 			/* Android */
 			(self::is('android') ? self::m . ' android' : ''),
 			/* Tablet */
